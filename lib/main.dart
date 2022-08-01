5d81223fab89a79641132f2f36517e5e88b708e6
Unekwe Francis <unekwenchekwube@gmail.com>
2022-08-01 20:27:30 +0100

    created first stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,20 +10,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final randomWord = WordPair.random();
     return MaterialApp(
       // title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:  Center(
+          child: Text(randomWord.asSnakeCase),
         ),
       ),
     );
   }
 }
 
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
 
 
 // import 'package:flutter/material.dart';
