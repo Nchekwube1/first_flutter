c082be633d12004ca502300f3e0dc58f1ef84ae9
Unekwe Francis <unekwenchekwube@gmail.com>
2022-08-01 21:07:38 +0100

    finishing up

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Welcome to Flutter',
+             title: 'Startup name generator',
+
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup name generator'),
         ),
         body: const Center(
           child: RandomWords(),
