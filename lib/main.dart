23667481cb9f34c9df91adce09de041a3a145756
Unekwe Francis <unekwenchekwube@gmail.com>
2022-08-01 20:00:46 +0100

    added english_words open source package

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      // title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
